35548877eaba7d75c316b6111048f51c661cb367
Sonia Chemaa <sonia.chemaa@outlook.com>
2017-09-25 21:30:02 +0200

    files and folder created + view files created with form

---
+++ modele/get_newmember.php
@@ -0,0 +1,11 @@
+<?php
+
+// CONNEXION A LA BASE DE DONNEES VIA PDO
+
+try{
+$bdd = new PDO('mysql:host=localhost;dbname=test;charset=utf8', 'root', 'root', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+}
+catch (Exception $e)
+{
+  die('Erreur: '. $e->getMessage());
+}
