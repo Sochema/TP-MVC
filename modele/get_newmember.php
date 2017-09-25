1d3de844cdcafa4aa4a6ee8e119d1a8eca7aeea6
Sonia Chemaa <sonia.chemaa@outlook.com>
2017-09-25 21:51:30 +0200

    complementary files added

--- modele/get_newmember.php
+++ modele/get_newmember.php
@@ -1,11 +1,20 @@
 <?php
-
 // CONNEXION A LA BASE DE DONNEES VIA PDO
+function PDOconnect(){
+  try{
+    $bdd = new PDO('mysql:host=localhost;dbname=espacemembre;charset=utf8', 'root', 'root', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+  }
+  catch (Exception $e)
+  {
+    die('Erreur: '. $e->getMessage());
+  }
+}
 
-try{
-$bdd = new PDO('mysql:host=localhost;dbname=test;charset=utf8', 'root', 'root', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
-}
-catch (Exception $e)
-{
-  die('Erreur: '. $e->getMessage());
-}
+
+
+$req = $bdd -> prepare('INSERT INTO membre(pseudo, mdp, mail, date_inscription) VALUES (:pseudo, :mdp, :mail, CURDATE())');
+$req-> execute(array(
+  "pseudo" => $pseudo,
+  "mdp" => $pass_mdp,
+  "mail"=> $mail
+));
